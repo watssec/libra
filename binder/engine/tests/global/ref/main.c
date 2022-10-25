e2ff98f353ed18f2b7589c613a70fd64bf0dc6f1
Meng Xu <meng.xu.cs@uwaterloo.ca>
2022-10-25 01:28:33 -0400

    x

---
+++ binder/engine/tests/global/ref/main.c
@@ -0,0 +1,16 @@
+int g_int;
+int *g_ptr_int = &g_int;
+int *g_ptr_int2 = &g_int;
+
+long g_long;
+void *g_ptr_void[2] = {&g_int, &g_long};
+
+void **g_ptr_ptr_void = g_ptr_void;
+void *g_ptr_ptr_void_as_ptr = g_ptr_void;
+
+struct simple {
+  int *f_int;
+  long *g_long;
+  void **g_ptr_void;
+};
+struct simple g_simple = {&g_int, &g_long, g_ptr_void};
