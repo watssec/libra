05f3eed7ad01f52265435a6def03e52b2029ea46
Meng Xu <meng.xu.cs@uwaterloo.ca>
2023-09-04 18:32:53 -0300

    x

--- binder/testsuite/src/lib.rs
+++ binder/testsuite/src/lib.rs
@@ -1,16 +1,18 @@
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 use anyhow::{bail, Result};
 use structopt::StructOpt;
 
+use libra_shared::config::PATH_STUDIO;
 use libra_shared::dep::{DepState, Dependency};
+use libra_shared::logging;
 
 use crate::llvm::DepLLVMTestSuite;
 
 mod llvm;
 
 #[derive(StructOpt)]
-pub enum DepAction {
+enum Command {
     /// Build the dependency
     Build {
         /// Temporary directory for the build process
@@ -28,45 +30,70 @@ pub enum DepAction {
 }
 
 #[derive(StructOpt)]
-pub struct DepArgs {
-    /// Name of the deps
+#[structopt(
+    name = "libra-testsuite",
+    about = "A driver for LIBRA test suites",
+    rename_all = "kebab-case"
+)]
+struct Args {
+    /// Studio directory
+    #[structopt(short, long)]
+    studio: Option<PathBuf>,
+
+    /// Verbosity
+    #[structopt(short, long)]
+    verbose: bool,
+
+    /// Name of the test suite
     name: String,
 
-    /// Version of the deps (tag or branch)
+    /// Version of the test suite (tag or branch)
     #[structopt(short, long)]
     version: Option<String>,
 
     /// Subcommand
     #[structopt(subcommand)]
-    action: DepAction,
+    command: Command,
 }
 
-impl DepArgs {
-    fn run_internal<T: Dependency>(self, studio: &Path) -> Result<()> {
-        let Self {
-            name: _,
-            version,
-            action: command,
-        } = self;
-        let mut state: DepState<T> = DepState::new(studio, version.as_deref())?;
+/// Main entrypoint
+pub fn entrypoint() -> Result<()> {
+    let args = Args::from_args();
+    let Args {
+        studio,
+        verbose,
+        name,
+        version,
+        command,
+    } = args;
+    let studio = studio.as_ref().unwrap_or(&PATH_STUDIO);
 
-        match command {
-            DepAction::Build {
-                tmpdir,
-                config,
-                force,
-            } => {
-                state.build(tmpdir, config, force)?;
-            }
-        }
-        Ok(())
+    // setup logging
+    logging::setup(verbose)?;
+
+    // run the subcommand
+    match name.as_str() {
+        "llvm" => run_internal::<DepLLVMTestSuite>(studio, version.as_deref(), command)?,
+        _ => bail!("Invalid deps name: {}", name),
     }
 
-    pub fn run(self, studio: &Path) -> Result<()> {
-        let name = self.name.as_str();
-        match name {
-            "llvm" => self.run_internal::<DepLLVMTestSuite>(studio),
-            _ => bail!("Invalid deps name: {}", name),
+    Ok(())
+}
+
+fn run_internal<T: Dependency>(
+    studio: &Path,
+    version: Option<&str>,
+    command: Command,
+) -> Result<()> {
+    let state: DepState<T> = DepState::new(studio, version)?;
+    match command {
+        Command::Build {
+            tmpdir,
+            config,
+            force,
+        } => {
+            state.build(tmpdir, config, force)?;
         }
     }
+    Ok(())
 }
