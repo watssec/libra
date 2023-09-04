1a3d77f18443360d72f62c39518522aa290de117
Meng Xu <meng.xu.cs@uwaterloo.ca>
2023-09-04 11:31:53 -0300

    x

--- binder/engine/src/flow/shared.rs
+++ binder/engine/src/flow/shared.rs
@@ -1,4 +1,4 @@
-use std::ffi::OsStr;
+use std::ffi::{OsStr, OsString};
 use std::fs;
 use std::path::{Path, PathBuf};
 use std::process::Command;
@@ -11,10 +11,14 @@ use crate::EngineError;
 
 /// Context for all workflow
 pub struct Context {
-    /// Path to the opt tool
-    bin_opt: PathBuf,
+    /// Path to the clang compiler
+    bin_clang: PathBuf,
+    /// Path to the llvm-link tool
+    bin_llvm_link: PathBuf,
     /// Path to the llvm-dis tool
     bin_llvm_dis: PathBuf,
+    /// Path to the opt tool
+    bin_opt: PathBuf,
     /// Path to the libra pass
     lib_pass: PathBuf,
 }
@@ -24,8 +28,10 @@ impl Context {
         let pkg_llvm = Path::new(env!("LIBRA_CONST_LLVM_ARTIFACT"));
         let lib_pass = Path::new(env!("LIBRA_CONST_PASS_ARTIFACT"));
         Self {
-            bin_opt: pkg_llvm.join("bin").join("opt"),
+            bin_clang: pkg_llvm.join("bin").join("clang"),
+            bin_llvm_link: pkg_llvm.join("bin").join("llvm-link"),
             bin_llvm_dis: pkg_llvm.join("bin").join("llvm-dis"),
+            bin_opt: pkg_llvm.join("bin").join("opt"),
             lib_pass: lib_pass.to_path_buf(),
         }
     }
@@ -46,6 +52,36 @@ impl Context {
         Ok(())
     }
 
+    fn run_clang<I, S>(&self, input: &Path, output: &Path, args: I) -> Result<()>
+    where
+        I: IntoIterator<Item = S>,
+        S: AsRef<OsStr>,
+    {
+        let mut cmd = Command::new(&self.bin_clang);
+        cmd.args(args).arg("-o").arg(output).arg(input);
+        Self::run(cmd)
+    }
+
+    pub fn compile_to_bitcode<I, S>(&self, input: &Path, output: &Path, args: I) -> Result<()>
+    where
+        I: IntoIterator<Item = S>,
+        S: AsRef<OsStr>,
+    {
+        let mut flags = vec![OsString::from("-c"), OsString::from("-emit-llvm")];
+        flags.extend(args.into_iter().map(|i| i.as_ref().to_os_string()));
+        self.run_clang(input, output, flags)
+    }
+
+    pub fn link_bitcode<I, S>(&self, input: &[&Path], output: &Path, args: I) -> Result<()>
+    where
+        I: IntoIterator<Item = S>,
+        S: AsRef<OsStr>,
+    {
+        let mut cmd = Command::new(&self.bin_llvm_link);
+        cmd.args(args).arg("-o").arg(output).args(input);
+        Self::run(cmd)
+    }
+
     fn run_opt<I, S>(&self, input: &Path, output: Option<&Path>, args: I) -> Result<()>
     where
         I: IntoIterator<Item = S>,
@@ -59,16 +95,6 @@ impl Context {
         Self::run(cmd)
     }
 
-    /// Verify the consistency of the bitcode file
-    pub fn opt_verify(&self, input: &Path) -> Result<()> {
-        self.run_opt(&input, None, ["-passes=verify"])
-    }
-
-    /// Run a specified opt pipeline
-    pub fn opt_pipeline(&self, input: &Path, output: &Path, pipeline: &str) -> Result<()> {
-        self.run_opt(input, Some(output), [format!("--passes='{}'", pipeline)])
-    }
-
     /// Disassemble the bitcode file into readable format
     pub fn disassemble(&self, input: &Path, output: &Path) -> Result<()> {
         let mut cmd = Command::new(&self.bin_llvm_dis);
@@ -80,6 +106,16 @@ impl Context {
     pub fn disassemble_in_place(&self, input: &Path) -> Result<()> {
         let output = input.with_extension("ll");
         self.disassemble(input, &output)
+    }
+
+    /// Verify the consistency of the bitcode file
+    pub fn opt_verify(&self, input: &Path) -> Result<()> {
+        self.run_opt(input, None, ["-passes=verify"])
+    }
+
+    /// Run a specified opt pipeline
+    pub fn opt_pipeline(&self, input: &Path, output: &Path, pipeline: &str) -> Result<()> {
+        self.run_opt(input, Some(output), [format!("--passes='{}'", pipeline)])
     }
 
     /// Serialize a bitcode file to JSON
