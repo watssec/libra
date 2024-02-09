9644e1662aec93f874faa2e0188ce3dac1b806ad
Meng Xu <meng.xu.cs@uwaterloo.ca>
2024-02-08 21:21:19 -0500

    x

--- binder/example/src/bin/clang_wrap.rs
+++ binder/example/src/bin/clang_wrap.rs
@@ -1,14 +1,154 @@
 use std::process::Command;
 use std::{env, process};
 
+use serde::{Deserialize, Serialize};
+
 use libra_engine::flow::shared::Context;
 
+#[derive(Serialize, Deserialize)]
+enum ClangArg {
+    /// -c
+    ModeCompile,
+    /// -std=<token>
+    Standard(String),
+    /// -D<token>
+    Define(String),
+    /// -I <token>
+    Include(String),
+    /// -isysroot <token>
+    IncludeSysroot(String),
+    /// -O<level>
+    Optimization(String),
+    /// -arch <token>
+    Arch(String),
+    /// -march=<token>
+    MachineArch(String),
+    /// -g, --debug
+    Debug,
+    /// -mllvm -<key>{=<value>}
+    Backend(String, Option<String>),
+    /// -f<key>{=<value>}
+    Flag(String, Option<String>),
+    /// -W<key>{=<value>}
+    Warning(String, Option<String>),
+    /// -w, --no-warnings
+    NoWarnings,
+    /// -pthread
+    POSIXThread,
+    /// -o <token>
+    Output(String),
+    /// <token>
+    Input(String),
+}
+
+impl ClangArg {
+    fn parse<'a, I>(token: &'a str, stream: &mut I) -> Self
+    where
+        I: Iterator<Item = &'a str>,
+    {
+        if !token.starts_with('-') {
+            return Self::Input(token.to_string());
+        }
+
+        match token {
+            "-c" => {
+                return Self::ModeCompile;
+            }
+            "-I" => {
+                return Self::Include(Self::expect_next(stream));
+            }
+            "-arch" => {
+                return Self::Arch(Self::expect_next(stream));
+            }
+            "-g" | "--debug" => {
+                return Self::Debug;
+            }
+            "-isysroot" => {
+                return Self::IncludeSysroot(Self::expect_next(stream));
+            }
+            "-mllvm" => {
+                let (k, v) = Self::expect_maybe_key_value(&Self::expect_next(stream));
+                return Self::Backend(k, v);
+            }
+            "-w" | "--no-warnings" => {
+                return Self::NoWarnings;
+            }
+            "-pthread" => {
+                return Self::POSIXThread;
+            }
+            "-o" => {
+                return Self::Output(Self::expect_next(stream));
+            }
+            _ => (),
+        }
+
+        if let Some(inner) = token.strip_prefix("-std=") {
+            return Self::Standard(inner.to_string());
+        }
+        if let Some(inner) = token.strip_prefix("-D") {
+            return Self::Define(inner.to_string());
+        }
+        if let Some(inner) = token.strip_prefix("-I") {
+            return Self::Include(inner.to_string());
+        }
+        if let Some(inner) = token.strip_prefix("-O") {
+            return Self::Optimization(inner.to_string());
+        }
+        if let Some(inner) = token.strip_prefix("-march=") {
+            return Self::MachineArch(inner.to_string());
+        }
+        if let Some(inner) = token.strip_prefix("-f") {
+            let (k, v) = Self::expect_maybe_key_value(inner);
+            return Self::Flag(k, v);
+        }
+        if let Some(inner) = token.strip_prefix("-W") {
+            let (k, v) = Self::expect_maybe_key_value(inner);
+            return Self::Warning(k, v);
+        }
+
+        panic!("unknown Clang option: {}", token);
+    }
+
+    fn strip_prefix(cur: &str, prefix: &str) -> String {
+        cur.strip_prefix(prefix).unwrap().to_string()
+    }
+
+    fn expect_next<'a, I>(stream: &mut I) -> String
+    where
+        I: Iterator<Item = &'a str>,
+    {
+        stream.next().expect("token").to_string()
+    }
+
+    fn expect_maybe_key_value(item: &str) -> (String, Option<String>) {
+        match item.find('=') {
+            None => (item.to_string(), None),
+            Some(index) => {
+                let (key, val) = item.split_at(index);
+                let val = val.strip_prefix('=').unwrap();
+                (key.to_string(), Some(val.to_string()))
+            }
+        }
+    }
+}
+
 fn main() {
+    // get paths
     let ctxt = Context::new().expect("LLVM context");
     let bin_clang = ctxt.path_llvm(["bin", "clang"]).expect("ascii path only");
+
+    // collect arguments
+    let args: Vec<_> = env::args().skip(1).collect();
+
+    // pass-through the arguments and execute the command first
     let status = Command::new(bin_clang)
-        .args(env::args().skip(1))
+        .args(&args)
         .status()
         .expect("command execution");
-    process::exit(status.code().expect("status code"))
+    if !status.success() {
+        process::exit(status.code().expect("status code"))
+    }
+
+    // only process arguments upon successful invocation
+    args.into_iter();
 }
