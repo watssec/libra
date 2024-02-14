1fb28815918ee993aa33a71af53300c189356477
Meng Xu <meng.xu.cs@uwaterloo.ca>
2024-02-14 11:44:31 -0500

    another trial

--- binder/builder/src/deps/llvm.cmake
+++ binder/builder/src/deps/llvm.cmake
@@ -1,5 +1,5 @@
 # General
-set(CMAKE_BUILD_TYPE RELEASE CACHE STRING "")
+set(CMAKE_BUILD_TYPE Release CACHE STRING "")
 
 # Stage 1 setup
 set(CLANG_ENABLE_BOOTSTRAP ON CACHE BOOL "")
@@ -14,21 +14,17 @@ set(CLANG_BOOTSTRAP_TARGETS
     stage3-check-all
     stage3-check-llvm
     stage3-check-clang
-    stage3-install
-    stage3-test-suite CACHE STRING "")
+    stage3-test-suite
+    stage3-install CACHE STRING "")
 
-# Stage 1 goal: clang -stdlib=libc++
-set(STAGE1_PROJECTS "clang;lld;libc")
-set(STAGE1_RUNTIMES "compiler-rt;libcxx;libcxxabi;libunwind")
+# Stage 1 goal: build clang and lld with system cc
+#               the new clang has -fuse-ld=lld support
+set(STAGE1_PROJECTS "clang;lld")
+set(STAGE1_RUNTIMES "")
 
 set(LLVM_TARGETS_TO_BUILD Native CACHE STRING "")
 set(LLVM_ENABLE_PROJECTS ${STAGE1_PROJECTS} CACHE STRING "")
 set(LLVM_ENABLE_RUNTIMES ${STAGE1_RUNTIMES} CACHE STRING "")
-set(LLVM_ENABLE_LIBCXX ON CACHE BOOL "")
-set(CLANG_DEFAULT_CXX_STDLIB "libc++" CACHE STRING "")
-set(CLANG_DEFAULT_RTLIB "compiler-rt" CACHE STRING "")
-set(CLANG_DEFAULT_UNWINDLIB "libunwind" CACHE STRING "")
-set(LIBC_ENABLE_USE_BY_CLANG ON CACHE BOOL "")
 
 # Stage 2 setup
 set(BOOTSTRAP_CLANG_ENABLE_BOOTSTRAP ON CACHE STRING "")
@@ -36,10 +32,12 @@ set(BOOTSTRAP_CLANG_BOOTSTRAP_TARGETS
     clang
     check-all
     check-llvm
-    check-clang CACHE STRING "")
+    check-clang
+    test-suite CACHE STRING "")
 
-# Stage 2 goal: clang -stdlib=libc++ -flto=full
-set(STAGE2_PROJECTS "clang;lld;libc")
+# Stage 2 goal: build basic llvm tools with stage1-clang -flto=full
+#               the new clang has -fuse-ld=lld and -stdlib=libc++ support
+set(STAGE2_PROJECTS "clang;libc;lld")
 set(STAGE2_RUNTIMES "compiler-rt;libcxx;libcxxabi;libunwind")
 
 set(BOOTSTRAP_LLVM_TARGETS_TO_BUILD Native CACHE STRING "")
@@ -53,7 +51,8 @@ set(BOOTSTRAP_CLANG_DEFAULT_RTLIB "compiler-rt" CACHE STRING "")
 set(BOOTSTRAP_CLANG_DEFAULT_UNWINDLIB "libunwind" CACHE STRING "")
 set(BOOTSTRAP_LIBC_ENABLE_USE_BY_CLANG ON CACHE BOOL "")
 
-# Stage 3 goal: clang -stdlib=libc++ -flto=full and other tools and options
+# Stage 3 goal: build extra llvm tools with stage2-clang -flto=full
+#               the new clang has -fuse-ld=lld and -stdlib=libc++ support
 set(STAGE3_PROJECTS "clang;clang-tools-extra;libc;lld;lldb;polly")
 set(STAGE3_RUNTIMES "compiler-rt;libcxx;libcxxabi;libunwind")
 
