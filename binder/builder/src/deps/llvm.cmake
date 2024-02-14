da1dc04928d86c3125201d3db814380449eae23e
Meng Xu <meng.xu.cs@uwaterloo.ca>
2024-02-14 15:26:11 -0500

    x

--- binder/builder/src/deps/llvm.cmake
+++ binder/builder/src/deps/llvm.cmake
@@ -17,9 +17,9 @@ set(CLANG_BOOTSTRAP_TARGETS
     stage3-test-suite
     stage3-install CACHE STRING "")
 
-# Stage 1: build clang and lld with system cc
-#          the new clang has -fuse-ld=lld support
-set(STAGE1_PROJECTS "clang;lld")
+# Stage 1: build clang with system cc
+#          the new clang has -fuse-ld=* option
+set(STAGE1_PROJECTS "clang")
 set(STAGE1_RUNTIMES "")
 
 set(LLVM_TARGETS_TO_BUILD Native CACHE STRING "")
@@ -32,23 +32,20 @@ set(BOOTSTRAP_CLANG_BOOTSTRAP_TARGETS
     clang
     check-all
     check-llvm
-    check-clang
-    test-suite CACHE STRING "")
+    check-clang CACHE STRING "")
 
-# Stage 2: build basic llvm tools with stage1-clang -flto=full
-#          the new clang has -fuse-ld=lld and -stdlib=libc++ support
-set(STAGE2_PROJECTS "clang;libc;lld")
-set(STAGE2_RUNTIMES "compiler-rt;libcxx;libcxxabi;libunwind")
+# Stage 2: build clang and lld with stage1-clang
+#          the new clang has -fuse-ld=lld support
+set(STAGE2_PROJECTS "clang;lld")
+set(STAGE2_RUNTIMES "")
 
 set(BOOTSTRAP_LLVM_TARGETS_TO_BUILD Native CACHE STRING "")
 set(BOOTSTRAP_LLVM_ENABLE_PROJECTS ${STAGE2_PROJECTS} CACHE STRING "")
 set(BOOTSTRAP_LLVM_ENABLE_RUNTIMES ${STAGE2_RUNTIMES} CACHE STRING "")
-set(BOOTSTRAP_LLVM_ENABLE_LLD ON CACHE BOOL "")
-set(BOOTSTRAP_LLVM_ENABLE_LTO "Full" CACHE STRING "")
 
-# Stage 3: build extra llvm tools with stage2-clang -flto=full -stdlib=libc++
+# Stage 3: build the entire llvm with stage2-clang -flto=full
 #          the new clang has -fuse-ld=lld and -stdlib=libc++ support
-set(STAGE3_PROJECTS "clang;clang-tools-extra;libc;lld;lldb;polly")
+set(STAGE3_PROJECTS "clang;clang-tools-extra;lld;lldb;polly;libc")
 set(STAGE3_RUNTIMES "compiler-rt;libcxx;libcxxabi;libunwind")
 
 set(BOOTSTRAP_BOOTSTRAP_LLVM_TARGETS_TO_BUILD Native CACHE STRING "")
@@ -57,8 +54,3 @@ set(BOOTSTRAP_BOOTSTRAP_LLVM_ENABLE_RUNTIMES ${STAGE3_RUNTIMES} CACHE STRING "")
 set(BOOTSTRAP_BOOTSTRAP_LLVM_ENABLE_LLD ON CACHE BOOL "")
 set(BOOTSTRAP_BOOTSTRAP_LLVM_ENABLE_LTO "Full" CACHE STRING "")
 set(BOOTSTRAP_BOOTSTRAP_LLVM_ENABLE_RTTI ON CACHE BOOL "")
-set(BOOTSTRAP_BOOTSTRAP_LLVM_ENABLE_LIBCXX ON CACHE BOOL "")
-set(BOOTSTRAP_BOOTSTRAP_CLANG_DEFAULT_CXX_STDLIB "libc++" CACHE STRING "")
-set(BOOTSTRAP_BOOTSTRAP_CLANG_DEFAULT_RTLIB "compiler-rt" CACHE STRING "")
-set(BOOTSTRAP_BOOTSTRAP_CLANG_DEFAULT_UNWINDLIB "libunwind" CACHE STRING "")
-set(BOOTSTRAP_BOOTSTRAP_LIBC_ENABLE_USE_BY_CLANG ON CACHE BOOL "")
