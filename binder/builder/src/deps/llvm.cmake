1bbfa4ac6a788e16190b055e5e35fac40c5a576b
Meng Xu <meng.xu.cs@uwaterloo.ca>
2024-02-14 11:59:26 -0500

    x

--- binder/builder/src/deps/llvm.cmake
+++ binder/builder/src/deps/llvm.cmake
@@ -17,8 +17,8 @@ set(CLANG_BOOTSTRAP_TARGETS
     stage3-test-suite
     stage3-install CACHE STRING "")
 
-# Stage 1 goal: build clang and lld with system cc
-#               the new clang has -fuse-ld=lld support
+# Stage 1: build clang and lld with system cc
+#          the new clang has -fuse-ld=lld support
 set(STAGE1_PROJECTS "clang;lld")
 set(STAGE1_RUNTIMES "")
 
@@ -35,8 +35,8 @@ set(BOOTSTRAP_CLANG_BOOTSTRAP_TARGETS
     check-clang
     test-suite CACHE STRING "")
 
-# Stage 2 goal: build basic llvm tools with stage1-clang -flto=full
-#               the new clang has -fuse-ld=lld and -stdlib=libc++ support
+# Stage 2: build basic llvm tools with stage1-clang -flto=full
+#          the new clang has -fuse-ld=lld and -stdlib=libc++ support
 set(STAGE2_PROJECTS "clang;libc;lld")
 set(STAGE2_RUNTIMES "compiler-rt;libcxx;libcxxabi;libunwind")
 
@@ -45,14 +45,13 @@ set(BOOTSTRAP_LLVM_ENABLE_PROJECTS ${STAGE2_PROJECTS} CACHE STRING "")
 set(BOOTSTRAP_LLVM_ENABLE_RUNTIMES ${STAGE2_RUNTIMES} CACHE STRING "")
 set(BOOTSTRAP_LLVM_ENABLE_LLD ON CACHE BOOL "")
 set(BOOTSTRAP_LLVM_ENABLE_LTO "Full" CACHE STRING "")
-set(BOOTSTRAP_LLVM_ENABLE_LIBCXX ON CACHE BOOL "")
 set(BOOTSTRAP_CLANG_DEFAULT_CXX_STDLIB "libc++" CACHE STRING "")
 set(BOOTSTRAP_CLANG_DEFAULT_RTLIB "compiler-rt" CACHE STRING "")
 set(BOOTSTRAP_CLANG_DEFAULT_UNWINDLIB "libunwind" CACHE STRING "")
 set(BOOTSTRAP_LIBC_ENABLE_USE_BY_CLANG ON CACHE BOOL "")
 
-# Stage 3 goal: build extra llvm tools with stage2-clang -flto=full
-#               the new clang has -fuse-ld=lld and -stdlib=libc++ support
+# Stage 3: build extra llvm tools with stage2-clang -flto=full -stdlib=libc++
+#          the new clang has -fuse-ld=lld and -stdlib=libc++ support
 set(STAGE3_PROJECTS "clang;clang-tools-extra;libc;lld;lldb;polly")
 set(STAGE3_RUNTIMES "compiler-rt;libcxx;libcxxabi;libunwind")
 
