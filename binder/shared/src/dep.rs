4e95b39fb7bac49d5df483d51f9cc98e0cb0168a
Meng Xu <meng.xu.cs@uwaterloo.ca>
2024-09-24 23:38:50 -0400

    simplify deps

--- binder/shared/src/dep.rs
+++ binder/shared/src/dep.rs
@@ -2,64 +2,43 @@ use std::fs;
 use std::marker::PhantomData;
 use std::path::{Path, PathBuf};
 
-use anyhow::{anyhow, bail, Result};
+use anyhow::{bail, Result};
 use log::{info, warn};
 use tempfile::tempdir;
 
-use crate::config::{PATH_ROOT, PATH_STUDIO};
-use crate::git::GitRepo;
+use crate::config::PATH_STUDIO;
 
 /// A mark for dep state
 static READY_MARK: &str = "ready";
 
-/// A trait that marks an artifact resolver
-pub trait Resolver: Sized {
-    /// Construct a resolver from the baseline path
-    fn construct(path: PathBuf) -> Self;
-
-    /// Destruct the resolver and get back the baseline path
-    fn destruct(self) -> PathBuf;
-
-    /// Try to create a resolver from the baseline path
-    fn seek() -> Result<(GitRepo, Self)>;
-}
-
 /// A trait that marks a dependency in the project
-pub trait Dependency<R: Resolver> {
-    /// Location of the git repo from the project root
-    fn repo_path_from_root() -> &'static [&'static str];
+pub trait Dependency {
+    /// Name of this dependency
+    fn name() -> &'static str;
 
-    /// List configurable options for building
-    fn list_build_options(path_src: &Path, path_config: &Path) -> Result<()>;
+    /// Print information (e.g., configurable options) on how to build it
+    fn help(path_wks: &Path) -> Result<()>;
 
-    /// Build the deps from scratch
-    fn build(path_src: &Path, resolver: &R) -> Result<()>;
+    /// Build this dependency from scratch
+    fn build(path_wks: &Path) -> Result<()>;
 }
 
 /// A struct that represents the build-from-scratch state
-pub struct Scratch<R: Resolver, T: Dependency<R>> {
-    repo: GitRepo,
-    artifact: PathBuf,
-    _phantom_r: PhantomData<R>,
-    _phantom_t: PhantomData<T>,
+pub struct Scratch<T: Dependency> {
+    path_wks: PathBuf,
+    _phantom: PhantomData<T>,
 }
 
-impl<R: Resolver, T: Dependency<R>> Scratch<R, T> {
-    /// Build the deps from scratch
-    pub fn make(self) -> Result<Package<R, T>> {
-        let Self {
-            repo,
-            artifact,
-            _phantom_r,
-            _phantom_t,
-        } = self;
+impl<T: Dependency> Scratch<T> {
+    /// Build a dependency from scratch
+    pub fn make(self) -> Result<Package<T>> {
+        let Self { path_wks, _phantom } = self;
 
-        let mark = artifact.with_extension(READY_MARK);
+        let mark = path_wks.with_extension(READY_MARK);
 
         // build the artifact
-        fs::create_dir_all(&artifact)?;
-        let resolver = R::construct(artifact);
-        T::build(repo.path(), &resolver)?;
+        fs::create_dir_all(&path_wks)?;
+        T::build(&path_wks)?;
 
         // create the mark
         fs::OpenOptions::new()
@@ -69,103 +48,73 @@ impl<R: Resolver, T: Dependency<R>> Scratch<R, T> {
 
         // return the package
         Ok(Package {
-            repo,
-            artifact: resolver,
+            path_wks,
             _phantom: PhantomData,
         })
     }
 }
 
 /// A struct that represents the package-ready state
-pub struct Package<R: Resolver, T: Dependency<R>> {
-    repo: GitRepo,
-    artifact: R,
+pub struct Package<T: Dependency> {
+    path_wks: PathBuf,
     _phantom: PhantomData<T>,
 }
 
-impl<R: Resolver, T: Dependency<R>> Package<R, T> {
+impl<T: Dependency> Package<T> {
     /// Destroy the deps so that we can build it again
-    pub fn destroy(self) -> Result<Scratch<R, T>> {
-        let Self {
-            repo,
-            artifact,
-            _phantom,
-        } = self;
-
-        let path_artifact = artifact.destruct();
+    pub fn destroy(self) -> Result<Scratch<T>> {
+        let Self { path_wks, _phantom } = self;
 
         // remove the mark
-        let mark = path_artifact.with_extension(READY_MARK);
+        let mark = path_wks.with_extension(READY_MARK);
         if !mark.exists() {
-            bail!(
-                "package artifact exists without mark: {}/{}",
-                T::repo_path_from_root().join("/"),
-                repo.commit()
-            );
+            bail!("package artifact exists without mark: {}", T::name());
         }
         fs::remove_file(mark)?;
 
         // remove the artifact directory
-        fs::remove_dir_all(&path_artifact)?;
+        fs::remove_dir_all(&path_wks)?;
 
         // return the scratch
         Ok(Scratch {
-            repo,
-            artifact: path_artifact,
-            _phantom_r: PhantomData,
-            _phantom_t: PhantomData,
+            path_wks,
+            _phantom: PhantomData,
         })
     }
 }
 
 /// Automatically differentiate the scratch and package version of LLVM
-pub enum DepState<R: Resolver, T: Dependency<R>> {
-    Scratch(Scratch<R, T>),
-    Package(Package<R, T>),
+pub enum DepState<T: Dependency> {
+    Scratch(Scratch<T>),
+    Package(Package<T>),
 }
 
-impl<R: Resolver, T: Dependency<R>> DepState<R, T> {
+impl<T: Dependency> DepState<T> {
     /// Get the deps state
     pub fn new() -> Result<Self> {
         // derive the correct path
-        let segments = T::repo_path_from_root();
-
-        let mut repo_path = PATH_ROOT.clone();
-        repo_path.extend(segments);
-        let repo = GitRepo::new(repo_path, None)?;
-        let commit = repo.commit();
-
-        let mut artifact = PATH_STUDIO.to_path_buf();
-        artifact.extend(segments);
-        artifact.push(commit);
+        let path_wks = PATH_STUDIO.join(T::name());
 
         // a filesystem mark showing that the artifact is ready
-        let ready = artifact.with_extension(READY_MARK);
+        let mark = path_wks.with_extension(READY_MARK);
 
         // derive the state
-        let state = if ready.exists() {
-            if !artifact.exists() {
-                bail!(
-                    "package mark exists without artifact: {}/{}",
-                    segments.join("/"),
-                    commit
-                );
+        let state = if mark.exists() {
+            if !path_wks.exists() {
+                bail!("package mark exists without artifact: {}", T::name());
             }
             Self::Package(Package {
-                repo,
-                artifact: R::construct(artifact),
+                path_wks,
                 _phantom: PhantomData,
             })
         } else {
-            if artifact.exists() {
-                info!("Deleting unsuccessful build");
-                fs::remove_dir_all(&artifact)?;
+            if path_wks.exists() {
+                info!("Deleting previous build");
+                fs::remove_dir_all(&path_wks)?;
             }
             Self::Scratch(Scratch {
-                repo,
-                artifact,
-                _phantom_r: PhantomData,
-                _phantom_t: PhantomData,
+                path_wks,
+                _phantom: PhantomData,
             })
         };
 
@@ -173,18 +122,12 @@ impl<R: Resolver, T: Dependency<R>> DepState<R, T> {
         Ok(state)
     }
 
-    /// List the possible build options
-    pub fn list_build_options(self) -> Result<()> {
-        let repo = match self {
-            Self::Scratch(Scratch { repo, .. }) => repo,
-            Self::Package(Package { repo, .. }) => repo,
-        };
-
+    /// Print information (e.g., configurable options) on how to build it
+    pub fn help(self) -> Result<()> {
         // always happens in tmpfs
         let tmp = tempdir()?;
-        T::list_build_options(repo.path(), tmp.path())?;
+        T::help(tmp.path())?;
         tmp.close()?;
-
         Ok(())
     }
 
@@ -204,13 +147,5 @@ impl<R: Resolver, T: Dependency<R>> DepState<R, T> {
         };
         scratch.make()?;
         Ok(())
-    }
-
-    /// Retrieve the source and artifact
-    pub fn into_source_and_artifact(self) -> Result<(GitRepo, R)> {
-        match self {
-            Self::Scratch(_) => Err(anyhow!("package not ready")),
-            Self::Package(Package { repo, artifact, .. }) => Ok((repo, artifact)),
-        }
     }
 }
